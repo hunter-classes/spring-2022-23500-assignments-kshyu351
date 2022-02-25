01afa06850c0fbacf5e6b62a19d2bf0a37742027
Kirsten Shyu <kirsten.shyu00@myhunter.cuny.edu>
2022-02-25 02:30:19 -0500

    finish

---
+++ assignments/recursive-search/knightstour.cpp
@@ -0,0 +1,121 @@
+#include <iostream> 
+
+#include <fstream>
+#include <string>
+#include <unistd.h>
+
+char knight = 'N'; 
+char wall=' ';
+char path='.';
+char visited = '#';
+int solutions[5][5]; 
+//std::string board[7][7];
+// int solutionsRow = 0; 
+// int solutionsCol = 0; 
+int sequence = 1; 
+
+
+int load_board(std::string filename, std::string *board){
+    //reads the board line by line 
+    // i represents num of lines 
+  std::ifstream infile(filename);
+  int i=0;
+  while(std::getline(infile,board[i])){
+    i++;
+  }
+  return i;
+}
+
+
+void print_board(std::string board[],int lines){
+  std::cout << "[0;0H\n";
+  for(int i = 0; i < lines; i++){
+    std::cout << board[i] << "\n";
+  }
+}
+
+
+void solve(std::string board[], int lines, int row, int col, bool &solved){
+  if (sequence == 26){
+    solved = true;
+    return;
+  }
+  if (board[row][col]==wall ||
+      board[row][col]==knight || board[row][col]==visited) {
+
+    return;
+  }
+
+  board[row][col]=knight;
+  usleep(80000);
+  print_board(board,lines);
+
+   
+  // solutions[solutionsRow][solutionsCol] = 5*(row-2) + col-1; 
+  solutions[row-2][col-2] = sequence;
+  sequence++; 
+  // int temp = 5*(row-2) + col-1;
+  // std::cout << temp << std::endl; 
+
+  // std::cout <<  solutions[solutionsRow][solutionsCol] << std::endl; 
+    
+  
+
+  //1st way 
+  if (!solved) solve(board,lines,row-2,col+1,solved);
+ 
+  //2nd 
+  if (!solved) solve(board,lines,row-1,col+2,solved);
+ 
+  //3rd
+  if (!solved) solve(board,lines,row+1,col+2,solved);
+  
+  //4th
+  if (!solved) solve(board,lines,row+2,col+1,solved);
+ 
+  //5th 
+  if (!solved) solve(board,lines,row+2,col-1,solved);
+ 
+  //6th
+  if (!solved) solve(board,lines,row+1,col-2,solved);
+  
+  //7th
+  if (!solved) solve(board,lines,row-1,col-2,solved);
+
+  //8th
+  if (!solved) solve(board,lines,row-2,col-1,solved);
+
+  if (!solved) board[row][col]=visited;
+}
+
+
+int main() {
+  std::string board[9];
+  int lines;  
+  lines = load_board("board.dat", board);
+
+  std::cout << "[2J;\n";
+  print_board(board,lines);
+  bool solved = false;
+  solve(board,lines,2,2,solved);
+  print_board(board,lines);
+  
+
+  for (int i = 0; i < 5; i++) { 
+      for (int j = 0; j < 5; j++) { 
+          std::cout << solutions[i][j] << ":";
+      }
+      std::cout << std::endl; 
+  }
+
+  std::cout << "Done!\n";
+}
+
+/*
+1   12  7   0   3
+6   0   2   11  8
+0   10  13  4   0
+14  5   9   14  14
+*/
+
+
